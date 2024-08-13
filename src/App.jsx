dae09f4ebd207dfa5083018addf7afd26728081c
alfianvx <devsroot8@gmail.com>
2024-08-13 11:43:29 +0700

    fix: add defaut route to /login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
@@ -6,6 +6,7 @@ import Profile from "./pages/Profile";
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/profile" element={<Profile />} />
